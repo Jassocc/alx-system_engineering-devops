33f863816a6462b9c77322367597ff4624cfe3af
Jassocc <clnearle@gmail.com>
2023-12-22 06:38:09 -0800

    task 1 update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,13 @@
 # install flask from pip3
-package { 'Flask':
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
   require  => Package['python3-pip'],
+}
+
+package {'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
 
 package { 'python3-pip':
