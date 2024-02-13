47dc32d898b66a16ce5e37953b91e0e568ebced3
Jassocc <clnearle@gmail.com>
2024-02-13 10:32:46 +0000

    ask 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,10 +12,9 @@ def number_of_subscribers(subreddit):
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "MyRedditClient/1.0"}
-    response = requests.get(url, headers=headers, allow_redirects=True)
-    if response.status_code == 404:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         return 0
-    else:
-        res = response.json().get("data")
-        aft = res.get("subscribers")
-        return aft
+    res = response.json().get("data")
+    aft = res.get("subscribers")
+    return aft
