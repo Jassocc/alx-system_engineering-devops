ac43c2bfe15eeab1e38cc928e460138310083893
Jassocc <clnearle@gmail.com>
2024-02-13 11:59:17 +0000

    task 3

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -15,7 +15,7 @@ def count_words(subreddit, word_list, after=None, counts=None):
     headers = {"User-Agent": "Mozilla/5.0"}
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     if after:
-         url += "?after={}".format(after)
+        url += "?after={}".format(after)
     response = requests.get(url, headers=headers)
     if response.status_code != 200:
         return
@@ -29,7 +29,8 @@ def count_words(subreddit, word_list, after=None, counts=None):
                 counts[word] = counts.get(word, 0) + title.count(word)
     next_page = data["data"]["after"]
     if next_page:
-        return count_words(subreddit, word_list, after=next_page, counts=counts)
+        return count_words(subreddit, word_list, after=next_page,
+                           counts=counts)
     sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
     for word, count in sorted_counts:
         print("{}: {}".format(word, count))
