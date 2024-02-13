0a6d50817c9615db3501421bd8b023eaab456bf9
Jassocc <clnearle@gmail.com>
2024-02-13 08:47:55 +0000

    task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Write a function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    returns the 10 hottest posts
+    """
+    headers = ({"User-Agent": "Mozilla/5.0"})
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        posts = data["data"]["children"]
+        if not posts:
+           print("None")
+        else:
+            for post in posts:
+                print(post["data"]["title"])
+    else:
+        print("None")
